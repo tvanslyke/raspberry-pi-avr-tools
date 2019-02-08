b4db05e941b1e0b7efc88dac9f78cfca5ed43377
Timothy Van Slyke <vanslyke.t@husky.neu.edu>
2019-02-08 14:39:12 -0500

    added flag to enable device libraries in avr-libc

--- install_avrlibc.sh
+++ install_avrlibc.sh
@@ -1,7 +1,7 @@
-tar -xf avr-libc-2.0.0.tar.bz2
-mkdir build-avrlibc
+# tar -xf avr-libc-2.0.0.tar.bz2
+# mkdir build-avrlibc
 cd build-avrlibc
-./../avr-libc-2.0.0/configure --build=`./config.guess` --host=avr
+./../avr-libc-2.0.0/configure --build=`./config.guess` --host=avr --enable-device-lib
 make -j 4
 sudo make install
 cd ..
